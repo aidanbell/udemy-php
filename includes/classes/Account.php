38bf30fec9693009087fb372e669d2c4e442f38a
aidanbell0 <aidanbell0182@gmail.com>
2020-02-03 15:37:18 -0500

    Section 3.

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,8 +1,10 @@
 <?php
   class Account {
+    private $con;
     private $errorArray;
 
-    public function __construct() {
+    public function __construct($con) {
+      $this->con = $con;
       $this->errorArray = array();
     }
 
@@ -15,7 +17,7 @@
 
       if(empty($this->errorArray)) {
         //Insert into db
-        return true;
+        return $this->insertUserDetails($un, $fn, $ln, $em, $pw);
       }
       else {
         return false;
@@ -27,6 +29,15 @@
         $error = '';
       }
       return "<span class='errorMessage'>$error</span>";
+    }
+
+    private function insertUserDetails($un, $fn, $ln, $em, $pw) {
+      $encryptedPw = md5($pw);
+      $profilePic = "assets/images/profile-pics/head_emerald.png";
+      $date = date("Y-m-d");
+
+      $result = mysqli_query($this->con, "INSERT INTO users VALUES ('', '$un', '$fn', '$ln', '$em', '$encryptedPw', '$date', '$profilePic')");
+      return $result;
     }
 
     private function validateUsername($un) {
@@ -65,7 +76,7 @@
         array_push($this->errorArray, Constants::$passwordsDoNotMatch);
         return;
       }
-      if(preg_match('/[^A-Za-Z0-9]/', $pw)) {
+      if(preg_match('/[^A-Za-z0-9]/', $pw)) {
         array_push($this->errorArray, Constants::$passwordNotAlphaNumeric);
         return;
       }
