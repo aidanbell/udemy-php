625e0d513635532091383c7cc2fd512071fcc30f
aidanbell0 <aidanbell0182@gmail.com>
2020-02-05 15:26:38 -0500

    Section 6 Complete.

--- index.php
+++ index.php
@@ -21,64 +21,93 @@ else {
 </head>
 
 <body>
-	<div id="nowPlayingBarContainer">
-		<div id="nowPlayingBar">
-			<div id="nowPlayingLeft">
-				<div class="content">
-					<span class="albumLink">
-						<img class="albumArtwork" src="./assets/images/unnamed.jpg" alt="Album Art">
-					</span>
-					<div class="trackInfo">
-						<span class="trackName">
-							<span>Happy Birthday</span>
+	<div id="mainContainer">
+		<div id="topContainer">
+			<div id="navBarContainer">
+				<nav class="navBar">
+					<a href="index.php" class="logo">
+						<i class="material-icons">library_music</i>
+					</a>
+					<div class="group">
+						<div class="navItem">
+							<a href="search.php" class="navItemLink">Search
+								<i class="material-icons search">search</i>
+							</a>
+						</div>
+					</div>
+					<div class="group">
+						<div class="navItem">
+							<a href="browse.php" class="navItemLink">Browse</a>
+						</div>
+						<div class="navItem">
+							<a href="yourMusic.php" class="navItemLink">Your Music</a>
+						</div>
+						<div class="navItem">
+							<a href="profile.php" class="navItemLink">Profile</a>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</div>
+		<div id="nowPlayingBarContainer">
+			<div id="nowPlayingBar">
+				<div id="nowPlayingLeft">
+					<div class="content">
+						<span class="albumLink">
+							<img class="albumArtwork" src="./assets/images/unnamed.jpg" alt="Album Art">
 						</span>
-						<span class="artistName">
-							<span>Aidan Bell</span>
-						</span>
+						<div class="trackInfo">
+							<span class="trackName">
+								<span>Happy Birthday</span>
+							</span>
+							<span class="artistName">
+								<span>Aidan Bell</span>
+							</span>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div id="nowPlayingCenter">
-				<div class="content playerControls">
-					<div class="buttons">
-						<button type="button" name="button" class="controlButton shuffle" title="Shuffle Button">
-							<i class="material-icons">shuffle</i>
-						</button>
-						<button type="button" name="button" class="controlButton previous" title="Previous Button">
-							<i class="material-icons">skip_previous</i>
-						</button>
-						<button type="button" name="button" class="controlButton play" title="Play Button">
-							<i class="material-icons">play_circle_outline</i>
-						</button>
-						<button type="button" name="button" class="controlButton pause" title="Pause Button" style="display: none;">
-							<i class="material-icons">pause_circle_outline</i>
-						</button>
-						<button type="button" name="button" class="controlButton next" title="Next Button">
-							<i class="material-icons">skip_next</i>
-						</button>
-						<button type="button" name="button" class="controlButton repeat" title="Repeat Button">
-							<i class="material-icons">repeat</i>
-						</button>
+				<div id="nowPlayingCenter">
+					<div class="content playerControls">
+						<div class="buttons">
+							<button type="button" name="button" class="controlButton shuffle" title="Shuffle Button">
+								<i class="material-icons">shuffle</i>
+							</button>
+							<button type="button" name="button" class="controlButton previous" title="Previous Button">
+								<i class="material-icons">skip_previous</i>
+							</button>
+							<button type="button" name="button" class="controlButton play" title="Play Button">
+								<i class="material-icons">play_circle_outline</i>
+							</button>
+							<button type="button" name="button" class="controlButton pause" title="Pause Button" style="display: none;">
+								<i class="material-icons">pause_circle_outline</i>
+							</button>
+							<button type="button" name="button" class="controlButton next" title="Next Button">
+								<i class="material-icons">skip_next</i>
+							</button>
+							<button type="button" name="button" class="controlButton repeat" title="Repeat Button">
+								<i class="material-icons">repeat</i>
+							</button>
+						</div>
+						<div class="playbackBar">
+							<span class="progressTime current">0:00</span>
+							<div class="progressBar">
+								<div class="progressBarBg">
+									<div class="progress"></div>
+								</div>
+							</div>
+							<span class="progressTime remaining">0:00</span>
+						</div>
 					</div>
-					<div class="playbackBar">
-						<span class="progressTime current">0:00</span>
+				</div>
+				<div id="nowPlayingRight">
+					<div class="volumeBar">
+						<button class="controlButton volume" type="button" name="button" title="Volume Button">
+							<i class="material-icons">volume_up</i>
+						</button>
 						<div class="progressBar">
 							<div class="progressBarBg">
 								<div class="progress"></div>
 							</div>
-						</div>
-						<span class="progressTime remaining">0:00</span>
-					</div>
-				</div>
-			</div>
-			<div id="nowPlayingRight">
-				<div class="volumeBar">
-					<button class="controlButton volume" type="button" name="button" title="Volume Button">
-						<i class="material-icons">volume_up</i>
-					</button>
-					<div class="progressBar">
-						<div class="progressBarBg">
-							<div class="progress"></div>
 						</div>
 					</div>
 				</div>
